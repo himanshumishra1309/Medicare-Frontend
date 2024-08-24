1dd1b8a9d48195a80d5c7b32bf2b1df9ca47dfcf
himanshumishra1309 <himanshu.mishra0913@gmail.com>
2024-08-24 22:17:30 +0530

    made some changes

--- src/components/StudentSISU/StudentSISU.jsx
+++ src/components/StudentSISU/StudentSISU.jsx
@@ -89,7 +89,19 @@ function StudentSISU() {
         try {
             const hashedPassword = await bcrypt.hash(signupData.password, 10);
             const signupDataWithHash = { ...signupData, password: hashedPassword };
-            const response = await axios.post('https://medicare-backend-1.vercel.app/student', signupDataWithHash);
+            // const response = await axios.post('https://medicare-backend-1.vercel.app/student', signupDataWithHash, Header:);
+            const response = await axios({
+                method: "POST",
+                url: "https://medicare-backend-1.vercel.app/student",
+                data: signupDataWithHash,
+                responseType: 'json',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': 'https://medicare-frontend-lyart.vercel.app',
+                  // 'Access-Control-Request-Headers': '*',
+                },
+                withCredentials: true,
+              })
             localStorage.setItem('studentId', response.data.id);
             navigate('/app/pcp');
         } catch (error) {
